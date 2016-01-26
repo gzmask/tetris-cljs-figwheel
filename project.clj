cc8f7b85a98662a011bc044c00b0465359df3364
gzmask <gzmask@gmail.com>
2016-01-26 13:18:07 -0600

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject threejs-figwheel "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Do what the fuck you want to public license"
+            :url "www.wtfpl.net"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
