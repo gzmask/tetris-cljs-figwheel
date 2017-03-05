e373da8eec1b76c7852f702351d41a4b0be295b2
gzmask <gzmask@gmail.com>
2017-03-04 21:57:11 -0800

    fixed figwheel not working with BASH on windows problem by updated clj to 1.8

--- project.clj
+++ project.clj
@@ -6,12 +6,13 @@
 
   :min-lein-version "2.5.3"
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/core.async "0.2.374"
                   :exclusions [org.clojure/tools.reader]]]
 
-  :plugins [[lein-figwheel "0.5.0-6"]
+  :plugins [ ;[lein-figwheel "0.5.0-6"]
+            [lein-figwheel "0.5.9"]
             [lein-cljsbuild "1.1.2" :exclusions [[org.clojure/clojure]]]]
 
   :source-paths ["src"]
